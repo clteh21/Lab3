dbc9b4aed923a32e712b34f073ab91fcdab5c80f
{chonglin} <{tehchonglin@gmail.com}>
2022-05-06 16:05:48 +0800

    Version 1

--- Lab3.py
+++ Lab3.py
@@ -12,7 +12,14 @@ def bubble_sort(arr, sorting_order):
     # Get number of elements in the list
     n = len(arr_result)
 
-    if n < 10:
+    for i in range(n):
+        if type(arr[i])!= int:
+            p=0
+            return 3
+        else:
+            p=1
+
+    if n == 10 and p == 1:
         # Traverse through all array elements
         for i in range(n - 1):
             # range(n) also work but outer loop will
@@ -26,21 +33,24 @@ def bubble_sort(arr, sorting_order):
                         arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
 
 
-                elif sorting_order == SORT_DESCENDING:
+                if sorting_order == SORT_DESCENDING:
                     if arr_result[j] < arr_result[j + 1]:
                         arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
 
-                else:
-                    # Return an empty array
-                    arr_result = []
     else:
-        arr_result = -1
+        if n>10 and p == 1:
+            return 1
+        if n<10 and n>0 and p == 1:
+            return 2
+        if n==0:
+            return 0
+
 
     return arr_result
 
 def main():
     # Driver code to test above
-    arr = [64, 34, 25, 12, 22, 11, 90]
+    arr = [64, 34, 25, 12, 22, 11, 90, 55, 66, 77]
 
     # Sort in ascending order
     result = bubble_sort(arr, SORT_ASCENDING)
--- Test_Lab3.py
+++ Test_Lab3.py
@@ -5,8 +5,8 @@ print("Test_Lab3")
 
 def test_bubble_sort_ascending():
     result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
-    test_arr = [11, 12, 22, 25, 34, 64, 90]
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 55, 66, 77]
+    test_arr = [11, 12, 22, 25, 34, 55, 64, 66, 77, 90]
 
     result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
 
@@ -14,17 +14,41 @@ def test_bubble_sort_ascending():
 
 def test_bubble_sort_descending():
     result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
-    test_arr = [90, 64, 34, 25, 22, 12, 11]
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 55, 66, 77]
+    test_arr = [90, 77, 66, 64, 55, 34, 25, 22, 12, 11]
 
     result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
 
     assert (result == test_arr)
 
-def test_bubble_sort_invalid():
-    result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
+def test_req03():
+    arr = [64, 34, 25, 12, 22, 11, 90, 55]
+    sorting_order=0
 
-    result = Lab3.bubble_sort(input_arr, 3)
+    result = Lab3.bubble_sort(arr, sorting_order)
 
-    assert (result == [])
+    assert (result == 2)
+
+def test_req02():
+    arr = [64, 34, 25, 12, 22, 11, 90, 55, 66, 77, 88]
+    sorting_order=0
+
+    result = Lab3.bubble_sort(arr, sorting_order)
+
+    assert (result == 1)
+
+def test_req04():
+    arr = []
+    sorting_order=0
+
+    result = Lab3.bubble_sort(arr, sorting_order)
+
+    assert (result == 0)
+
+def test_req05():
+    arr = [64.4, 34, 25, 12, 22, 11, 90, 55, 66, 77]
+    sorting_order = 0
+
+    result = Lab3.bubble_sort(arr, sorting_order)
+
+    assert (result == 3)
